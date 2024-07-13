6be8c8df1ec6f16659dbc18c4518df3a7716ce88
souhail <ouargui550@gmail.com>
2024-07-13 23:13:23 +0100

    get and post a user working

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const usrRouter = express.Router();
+const User = require('../models/user');
+
+
 
 
 // getting all
 
 
-usrRouter.get('/',(req,res)=> {
-  res.send('hello world');
-    
+usrRouter.get('/',async(req,res)=> {
+  // res.send('hello world');
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({message: error.message})
+    }
 })
 
 
@@ -15,14 +23,28 @@ usrRouter.get('/',(req,res)=> {
 // getting one
 
 
-usrRouter.get('/:id',(req,res)=> {
-    res.send(req.params.id)});
+usrRouter.get('/:id',getUser,(req,res)=> {
+    res.send(res.user)
+  }
+  );
 
 // creating one
 
 
-usrRouter.post('/:id',(req,res)=> {
-  res.send(req.params.id);
+usrRouter.post('/',(req,res)=> {
+  // res.send(req.params.id);
+
+  const user = new User({
+    name: req.body.name,
+    role: req.body.role,
+  })
+  try {
+    const newUser = user.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({message: error.message});
+  }
+
 })
 
 // updating one
@@ -40,6 +62,28 @@ usrRouter.patch('/:id',(req,res)=> {
 usrRouter.delete('/:id',(req,res)=> {
     
 })
+
+
+
+async function getUser(req,res,next){
+  let user
+  try {
+    user = await User.findById(req.params.id)
+    if (user == null) {
+      return res.status(404).json({message: 'cannot find user'});
+      
+    }
+  } catch (error) {
+    
+    return res.status(500).json({message: error.message});
+  }
+
+res.user = user;
+next();
+
+}
+
+
 
 
 
